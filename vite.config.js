95482a49f5b2b8c64f1464fb1a9901ce17a43ac3
bargenal <bargenal@my.centennialcollege.ca>
2025-09-28 16:16:48 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/COMP229-Portfolio/"
+  base: "/comp229-portfolio/",
 })
